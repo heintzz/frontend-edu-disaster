cfd14bd2d98d2b14d23ec3fd308c64b9cb481738
heintzz <mhasnanr@gmail.com>
2024-05-20 13:28:24 +0700

    feat: modal on finish

--- app/eval/page.jsx
+++ app/eval/page.jsx
@@ -13,8 +13,10 @@ const caesarDressing = Caesar_Dressing({ subsets: ['latin'], weight: '400' });
 
 export default function EvalPage() {
   const router = useRouter();
+  
   const [answeredQuestions, setAnsweredQuestions] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isModalFinishedOpen, setIsModalFinishedOpen] = useState(false);
 
   const activeQuestion = questions[currentIndex];
   const [currentAnswer, setCurrentAnswer] = useState(null);
@@ -72,68 +74,93 @@ export default function EvalPage() {
   };
 
   return (
-    <div className="w-[100dvw] h-[100dvh] py-[3dvh] lg:py-[2dvh] px-[5dvh] bg-[#253333] text-white">
-      <div className="h-full flex flex-col gap-y-4 lg:gap-y-10 justify-between relative">
-        <p className={`${caesarDressing.className} lg:text-5xl text-center`}>EVALUASI</p>
-        <div className="bg-black rounded-[20px] h-full p-5 relative">
-          <button
-            className="absolute right-5 bottom-5 bg-[#EF473E] text-white font-bold p-1 text-sm lg:py-2 lg:px-4 rounded-[10px] lg:text-2xl"
-            onClick={() => {}}
-          >
-            <span className={caesarDressing.className}>Selesai</span>
-          </button>
-          <p className="text-sm lg:text-xl font-semibold">Soal no. {currentIndex + 1}</p>
-          <p className="text-sm my-3 lg:text-base lg:my-10">{activeQuestion.question}</p>
-          <div className="flex flex-col text-sm gap-y-4 lg:text-base lg:gap-y-8">
-            {activeQuestion.answers.map((answer, index) => {
-              const splits = answer.split('. ');
-              const [choice, text] = splits;
-              return (
-                <div className="flex gap-x-2 lg:gap-x-4 items-center" key={index}>
-                  <div
-                    role="button"
-                    className={`h-6 w-6 lg:h-8 lg:w-8 rounded-full grid place-items-center ${
-                      currentAnswer === choice
-                        ? ' bg-[#29ADB2] border-[#29ADB2]'
-                        : ' bg-[#EFEFEF]/10 border-[#EFEFEF]'
-                    }`}
-                    onClick={() => handleAnswerChange(choice)}
-                  >
-                    {choice}
+    <>
+      <div className="w-[100dvw] h-[100dvh] py-[3dvh] lg:py-[2dvh] px-[5dvh] bg-[#253333] text-white">
+        <div className="h-full flex flex-col gap-y-4 lg:gap-y-10 justify-between relative">
+          <p className={`${caesarDressing.className} lg:text-5xl text-center`}>EVALUASI</p>
+          <div className="bg-black rounded-[20px] h-full p-5 lg:p-10 relative">
+            <button
+              className="absolute right-5 bottom-5 bg-[#EF473E] text-white font-bold p-1 text-sm lg:py-2 lg:px-4 rounded-[10px] lg:text-2xl"
+              onClick={() => {}}
+            >
+              <span
+                className={caesarDressing.className}
+                onClick={() => setIsModalFinishedOpen(true)}
+              >
+                Selesai
+              </span>
+            </button>
+            <p className="text-sm lg:text-xl font-semibold">Soal no. {currentIndex + 1}</p>
+            <p className="text-sm my-3 lg:text-base lg:my-10">{activeQuestion.question}</p>
+            <div className="flex flex-col text-sm gap-y-4 lg:text-base lg:gap-y-8">
+              {activeQuestion.answers.map((answer, index) => {
+                const splits = answer.split('. ');
+                const [choice, text] = splits;
+                return (
+                  <div className="flex gap-x-2 lg:gap-x-4 items-center" key={index}>
+                    <div
+                      role="button"
+                      className={`h-6 w-6 lg:h-8 lg:w-8 rounded-full grid border border-[#EFEFEF] place-items-center ${
+                        currentAnswer === choice ? ' bg-[#29ADB2]' : ' bg-[#EFEFEF]/10'
+                      }`}
+                      onClick={() => handleAnswerChange(choice)}
+                    >
+                      {choice}
+                    </div>
+                    <p>{text}</p>
                   </div>
-                  <p>{text}</p>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
+          </div>
+          <div className="flex justify-center gap-x-3">
+            <button
+              className="flex items-center gap-x-2 bg-[#29ADB2] text-white font-bold p-1 text-sm lg:py-2 lg:px-4 rounded-[10px] lg:text-2xl"
+              onClick={() => {
+                if (currentIndex > 0) {
+                  setCurrentIndex((prev) => prev - 1);
+                } else {
+                  router.push('/');
+                }
+              }}
+            >
+              <Image src={arrowBack} alt="back icon" className="w-6 h-6" />
+              <span className={caesarDressing.className}>Kembali</span>
+            </button>
+            <button
+              className="flex items-center gap-x-2 bg-[#29ADB2] text-white font-bold p-1 text-sm lg:py-2 lg:px-4 rounded-[10px] lg:text-2xl"
+              onClick={() => {
+                if (currentIndex < questions.length - 1 && currentIndex >= 0) {
+                  setCurrentIndex((prev) => prev + 1);
+                }
+              }}
+            >
+              <span className={caesarDressing.className}>Selanjutnya</span>
+              <Image src={arrowNext} alt="back icon" className="w-6 h-6" />
+            </button>
           </div>
         </div>
-        <div className="flex justify-center gap-x-3">
-          <button
-            className="flex items-center gap-x-2 bg-[#29ADB2] text-white font-bold p-1 text-sm lg:py-2 lg:px-4 rounded-[10px] lg:text-2xl"
-            onClick={() => {
-              if (currentIndex > 0) {
-                setCurrentIndex((prev) => prev - 1);
-              } else {
-                router.push('/');
-              }
-            }}
-          >
-            <Image src={arrowBack} alt="back icon" className="w-6 h-6" />
-            <span className={caesarDressing.className}>Kembali</span>
-          </button>
-          <button
-            className="flex items-center gap-x-2 bg-[#29ADB2] text-white font-bold p-1 text-sm lg:py-2 lg:px-4 rounded-[10px] lg:text-2xl"
-            onClick={() => {
-              if (currentIndex < questions.length - 1 && currentIndex >= 0) {
-                setCurrentIndex((prev) => prev + 1);
-              }
-            }}
-          >
-            <span className={caesarDressing.className}>Selanjutnya</span>
-            <Image src={arrowNext} alt="back icon" className="w-6 h-6" />
-          </button>
-        </div>
       </div>
-    </div>
+      {isModalFinishedOpen && (
+        <div className="absolute w-full h-full top-0 text-white bg-black/50 backdrop-blur-[1px] grid place-items-center">
+          <div className="mx-4 w-full max-w-md rounded-lg p-6 shadow-lg bg-gray-900">
+            <div className="space-y-8">
+              <h2 className="text-lg">Sudah yakin dengan jawabanmu?</h2>
+              <div
+                className={`flex justify-end items-center gap-2 text-black ${caesarDressing.className}`}
+              >
+                <button
+                  className="px-3 py-1 rounded-md bg-gray-200"
+                  onClick={() => setIsModalFinishedOpen(false)}
+                >
+                  Batal
+                </button>
+                <button className="px-3 py-1 rounded-md bg-green-600">Submit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
